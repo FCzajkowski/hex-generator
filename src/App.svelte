2fefa4d5965b61943b5548e03afce1e2f6318388
Franciszek Czajkowski <FCzajkowski@proton.me>
2025-09-28 01:44:04 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let name;
+</script>
+<style lang="scss">
+	@import "./style.scss";
+	// I do this because I have no fucking clue how to import .scss file directly into svelte ;(
+</style>
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
