40f2aede102198ab141bc3582d55c2bd316520b5
Franciszek Czajkowski <FCzajkowski@proton.me>
2025-09-28 02:12:58 +0200

    first commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,52 @@
 <script>
-	export let name;
+	let hex = randomHex();
+
+	function randomHex() {
+	    return '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+	}
+
+	function change_color() {
+	    hex = randomHex();
+	}
 </script>
 <style lang="scss">
-	@import "./style.scss";
-	// I do this because I have no fucking clue how to import .scss file directly into svelte ;(
+	main{
+		height:100vh;
+		padding:0;
+		margin:0;
+		display:flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+
+	}
+	p{
+		font-size:30px;
+		color:white;
+		font-weight:900;
+
+	}
+	button{
+		padding:13px 15px;
+		background-color: transparent;
+		border: 2px solid white;
+		color:white;
+		border-radius:20px;
+		transition:250ms;
+		font-weight:600;
+	}
+	button:hover{
+		background-color: white;
+		color:black;
+
+		cursor:pointer;
+	}
 </style>
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main style="background-color: {hex};">
+	<p>
+		Your Random Color is: {hex}
+	</p>
+	<button on:click={change_color}>
+		Generate Random Number
+	</button>
 </main>
